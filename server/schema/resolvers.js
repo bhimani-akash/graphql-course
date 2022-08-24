9d285ee60983ec29f8ec62cb589db609a5e2e83b
bhimani-akash <bhimani.a@northeastern.edu>
2022-08-23 21:27:35 -0400

    added couple of resolvers and type defs

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,9 +1,33 @@
-const { UserList } = require("../FakeData");
+const { UserList, MovieList } = require("../FakeData");
+const _ = require("lodash");
 
 const resolvers = {
   Query: {
-    users() {
+    users: () => {
       return UserList;
+    },
+    user: (parent, args) => {
+      const id = args.id;
+      const user = _.find(UserList, { id: Number(id) });
+      return user;
+    },
+
+    movies: () => {
+      return MovieList;
+    },
+    movie: (parent, args) => {
+      const name = args.name;
+      const movie = _.find(MovieList, { name });
+      return movie;
+    },
+  },
+  User: {
+    favoriteMovies: () => {
+      return _.filter(
+        MovieList,
+        (movie) =>
+          movie.yearOfPublication >= 200 && movie.yearOfPublication <= 2010
+      );
     },
   },
 };
--- server/schema/type-defs.js
+++ server/schema/type-defs.js
@@ -1,16 +1,36 @@
 const { gql } = require("apollo-server");
 
 const typeDefs = gql`
+  enum Nationality {
+    CANADA
+    BRAZIL
+    INDIA
+    GERMANY
+    CHILE
+  }
+
   type User {
     id: ID!
     name: String!
     username: String!
     age: Int!
-    nationality: String!
+    nationality: Nationality!
+    friends: [User]
+    favoriteMovies: [Movie]
+  }
+
+  type Movie {
+    id: ID!
+    name: String!
+    yearOfPublication: Int!
+    isInTheaters: Boolean!
   }
 
   type Query {
     users: [User!]!
+    user(id: ID!): User!
+    movies: [Movie!]!
+    movie(name: String!): Movie!
   }
 `;
 
